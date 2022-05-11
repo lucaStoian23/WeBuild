c2a8e7e9a2e661122aeddd0f0c2cf81098ad7c13
lucastoian <lucastoian@outlook.com>
2022-05-11 12:08:35 +0200

    a

---
+++ src/test/java/pages/NuovoFornitore.java
@@ -0,0 +1,23 @@
+package test.java.pages;
+
+import main.java.Elements.BrowserElement;
+import org.openqa.selenium.By;
+
+public class NuovoFornitore {
+    public static final BrowserElement NazioneButton = new BrowserElement(new By[]{By.xpath("//div[@id='application-NPPSupplierCreation-Display-component---SupplierCreation--iNation-content']//div")});
+    public static final BrowserElement NazioneCerca = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--selectDialog-searchField-I")});
+    public static final BrowserElement Clessidra = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--selectDialog-searchField-search")});
+    public static final BrowserElement NazioneTrovata = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--_IDGenStandardListItem1-__clone20")});
+    public static final BrowserElement FormaGiuridicaButton = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--iGiurForm-arrow")});
+    public static final BrowserElement F = new BrowserElement(new By[]{By.id("")});
+    public static final BrowserElement ULFormeGiuridiche = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--iGiurForm-popup-list-listUl")});
+    public static final BrowserElement TipologiaFornitoreButton = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--iTipologia-arrow")});
+    public static final BrowserElement ULTipologieFornitore = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--iTipologia-popup-list-listUl")});
+    public static final BrowserElement RagioneSociale = new BrowserElement(new By[]{By.xpath("//input[@placeholder='Inserisci ragione sociale']")});
+    public static final BrowserElement PartitaIva = new BrowserElement(new By[]{By.xpath("//input[@placeholder='Inserisci la partita IVA...']")});
+    public static final BrowserElement CodiceFiscale = new BrowserElement(new By[]{By.xpath("//input[@placeholder='Codice fiscale...']")});
+    public static final BrowserElement PartitaIvaCEE = new BrowserElement(new By[]{By.xpath("//input[@placeholder='Inserisci la partita IVA comunitaria...']")});
+    public static final BrowserElement SearchInfoProviderButton = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--_IDGenButton1")});
+    public static final BrowserElement ULFornitoriTrovati = new BrowserElement(new By[]{By.id("_IDGenList1XX-listUl")});
+    public static final BrowserElement Crea = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierCreation--_IDGenButton3")});
+}
