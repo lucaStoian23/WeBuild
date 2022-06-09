07775f4ee37e28d1198b15e6f52c1709f183a8bc
f.cottone <f.cottone@reply.it>
2022-06-09 09:39:29 +0200

    feat-aggiunta-mappatura-OperationalOffice+initT023

---
+++ src/test/java/pages/OperationalOffices.java
@@ -0,0 +1,25 @@
+package test.java.pages;
+
+import main.java.Elements.BrowserElement;
+import org.openqa.selenium.By;
+
+public class OperationalOffices {
+    public static final BrowserElement ChooseOperationalOffices = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--ObjectPageLayout-anchBar-application-NPPSupplierCreation-Display-component---SupplierData--operationaloffices-anchor-content")});
+    public static final BrowserElement AddOperationalOffice = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--idNewUsrBtn-inner")});
+    //Elementi Create Operations Office
+    public static final BrowserElement OperationOfficeName = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeInputName-inner")});
+    public static final BrowserElement AddressTypeArrow = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeComboTipo-arrow")});
+    public static final BrowserElement AddressTypeUL = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeComboTipo-popup-list-listUl")});
+    public static final BrowserElement CountryButton = new BrowserElement(new By[]{By.id("SupplierCreateOfficeComboNation-vhi")});
+    public static final BrowserElement CountrySearchBar = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog-searchField-I")});
+    public static final BrowserElement CountryLents = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog-searchField-search")});
+    public static final BrowserElement CountryUL = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog-list-listUl")});
+    public static final BrowserElement Address = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeInputAddress-inner")});
+    public static final BrowserElement PostalCode = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeComboPostalCode-inner")});
+    public static final BrowserElement City = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeComboCity-inner")});
+    public static final BrowserElement ProvinceButton = new BrowserElement(new By[]{By.id("id_SupplierCreateOfficeLocal-vhi")});
+    public static final BrowserElement ProvinceSearch = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog2-searchField-I")});
+    public static final BrowserElement ProvinceLents = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog2-searchField-search")});
+    public static final BrowserElement ProvinceUL = new BrowserElement(new By[]{By.id("application-NPPSupplierCreation-Display-component---SupplierData--operationalofficesblock-Collapsed--selectDialog2-list-listUl")});
+    public static final BrowserElement Save = new BrowserElement(new By[]{By.id("_IDSupplierCreateOfficeCTAButtonSave-inner")});
+}
