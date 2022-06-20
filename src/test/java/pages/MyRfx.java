d0f62099d12de7b242ece7f0ce2685ef5d3cb06c
lucastoian <lucastoian@outlook.com>
2022-06-20 12:16:25 +0200

    ora clicca sul primo elemento della tabella

---
+++ src/test/java/pages/MyRfx.java
@@ -0,0 +1,8 @@
+package test.java.pages;
+
+import main.java.Elements.BrowserElement;
+import org.openqa.selenium.By;
+
+public class MyRfx {
+    public static final BrowserElement RFXtable = new BrowserElement(new By[]{By.xpath("//*[@id='application-NPPMyRFX-Display-component---MyRFX--IDMYRFXTable-tblBody']")});
+}
